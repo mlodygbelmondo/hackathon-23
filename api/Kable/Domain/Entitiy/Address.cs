7e74918e3ee243852b6ae5f0e274c6e782ca48dd
JakubWaj <jw303269@student.polsl.pl>
2023-12-02 19:37:46 +0100

    Removing Address entity

--- api/Kable/Domain/Entitiy/Address.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Entitiy;
-
-public class Address
-{
-    public int Id { get; set; }
-    public string Street { get; set; }
-    public string PostalCode { get; set; }
-    public string City { get; set; } 
-}
