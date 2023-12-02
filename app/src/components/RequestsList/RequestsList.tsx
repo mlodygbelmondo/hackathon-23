79fcded941c0c9a2394f16172b0f18670d97fb9e
Piotr Kaczorowski <piotr.kaczorowski@gorrion.io>
2023-12-03 00:13:20 +0100

    added requests list

--- app/src/components/RequestsList/RequestsList.tsx
+++ app/src/components/RequestsList/RequestsList.tsx
@@ -1,16 +1,22 @@
-import Image from "next/image";
+import type { Request } from "~/interfaces/common";
+import RequestCard from "./RequestCard";
 
-interface Props {}
-const RequestsList = ({}: Props) => {
+interface Props {
+  requests: Request[];
+}
+
+const RequestsList = ({ requests }: Props) => {
   return (
-    <div className="z-max flex h-16 w-16 items-center justify-center overflow-hidden rounded-full border">
-      <Image
-        src="https://www.nkt.com.pl/imgproxy/pvTKGdlM1jMXCaTNdveZkfkGvqW0F0bj4y-B_1VweQM/rt:fit/w:378/h:294/g:ce/ex:1/el:1/aHR0cHM6Ly9ua3Qud2lkZW4ubmV0L2NvbnRlbnQvMmV1cXgzNm1waS9wbmcvWURZxbxvXzN4MSw1X3MucG5nP2xhc3RNb2RpZmllZD1UdWUrTWFyKzA4KzE0JTNBMjElM0ExNitDRVQrMjAyMg.jpeg"
-        alt="halo"
-        className="z-1"
-        width={64}
-        height={64}
-      />
+    <div className="flex h-[540px] w-[45%] flex-col items-center gap-2 rounded-2xl border border-gray-200 bg-white pt-4 shadow-xl shadow-gray-100">
+      <p className="w-full px-8 text-left text-2xl font-bold text-gray-900">
+        Aktywne zgłoszenia
+      </p>
+      <div className="h-0.5 w-[90%] rounded-full bg-gray-200 px-8" />
+      <div className="flex h-full w-full flex-col items-center gap-2 overflow-auto px-8 pb-2 pt-1">
+        {requests.map((request) => (
+          <RequestCard request={request} />
+        ))}
+      </div>
     </div>
   );
 };
--- app/src/interfaces/common.ts
+++ app/src/interfaces/common.ts
@@ -1,15 +1,7 @@
-export interface Cable {
-  name: string;
-  link: string;
-  length: number;
-  type: string;
-  status: string;
-  color: string;
-}
-
 export interface Request {
   id: number;
   lat: number;
   lng: number;
-  cable: Cable;
+  cableName: string;
+  date: string;
 }
