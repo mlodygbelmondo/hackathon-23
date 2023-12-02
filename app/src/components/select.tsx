02fe17fe3a7ab9910ada00056035f43648906eeb
Jan Rapacz <janrapacz@MacBook-Pro-Jan.local>
2023-12-02 22:57:12 +0100

    janek/refactor form

---
+++ app/src/components/select.tsx
@@ -0,0 +1,47 @@
+export interface Option {
+  label: string;
+  value: string;
+}
+
+interface Props {
+  selectedValue: string;
+  options: Option[];
+  onChange: (value: string) => void;
+  label: string;
+  defaultOptionLabel?: string;
+}
+
+const Select: React.FunctionComponent<Props> = ({
+  label,
+  onChange,
+  options,
+  selectedValue,
+  defaultOptionLabel,
+}) => (
+  <div className="flex flex-col">
+    <label
+      htmlFor="type-of-metal"
+      className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+    >
+      {label}
+    </label>
+    <select
+      onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+        onChange(e.target.value);
+      }}
+      id="type-of-metal"
+      className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+    >
+      <option value="" selected={selectedValue === ""}>
+        {defaultOptionLabel}
+      </option>
+      {options.map((option) => (
+        <option selected={selectedValue === option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  </div>
+);
+
+export default Select;
--- app/src/pages/form/index.tsx
+++ app/src/pages/form/index.tsx
@@ -1,133 +1,440 @@
-import AmbientTemperature from "components/form/AmbientTemperature";
-import InstalationMethod from "components/form/InstallationMethod";
-import NumberOfLoadedVeins from "components/form/NumberOfLoadedVeins";
-import ThermalResistivityOfTheSoil from "components/form/ThermalResistivityOfTheSoil";
-import TypeOfInsulatingMaterial from "components/form/TypeOfInsulatingMaterial";
-import TypeOfMetalDetails from "components/form/TypeOfMetalDetails";
 import { useState } from "react";
+import Select, { type Option } from "~/components/select";
+import type {
+  CableEnvironment,
+  CableType,
+  ConductorLoad,
+  Environment,
+  InstallationType,
+} from "~/types";
 
-export interface initialDataProps {
+export interface InitialDataProps {
   typeOfMetal: string;
-  typeOfInsulatingMaterial: string;
+  installationLocation: string;
+  isolationMaterial: string;
   numberOfLoadedVeins: string;
-  instalationMethod: string;
+  installationMethod: string;
   ambientTemperature: string;
   thermalResistivityOfTheSoil: string;
 }
 
+const temperatures = [
+  "10",
+  "15",
+  "20",
+  "25",
+  "30",
+  "35",
+  "40",
+  "45",
+  "50",
+  "55",
+  "60",
+  "65",
+  "70",
+  "75",
+  "80",
+];
+
 const initialData = {
   typeOfMetal: "",
-  typeOfInsulatingMaterial: "",
+  installationLocation: "",
+  isolationMaterial: "",
   numberOfLoadedVeins: "",
-  instalationMethod: "",
+  installationMethod: "",
   ambientTemperature: "",
   thermalResistivityOfTheSoil: "",
 };
 
+const metalTypes = [
+  {
+    label: "Miedz",
+    value: "copper",
+  },
+  {
+    label: "Aluminium",
+    value: "aluminium",
+  },
+];
+
+const instalationLocations = [
+  {
+    label: "W ziemi",
+    value: "ground",
+  },
+  {
+    label: "W powietrzu",
+    value: "air",
+  },
+];
+
+const numberOfLoadedVeins = [
+  {
+    label: "układ jednofazowy (dwie żyły obciążone)",
+    value: "two",
+  },
+  {
+    label: "układ trójfazowy (wielozylowy)",
+    value: "multi",
+  },
+  {
+    label: "układ trójfazowy jednożyłowy",
+    value: "one",
+  },
+];
+
+const isolationMaterials = [
+  {
+    label: "PVC",
+    value: "PVC",
+  },
+  {
+    label: "XLPE",
+    value: "XLPE",
+  },
+  {
+    label: "B2ca",
+    value: "B2ca",
+  },
+];
+
+const cables: CableType[] = ["YDY", "YDYp", "YKY", "YKXS", "N2XH", "YAKXS"];
+
+const cablesEnvironments: CableEnvironment[] = [
+  {
+    name: "YDY",
+    availableEnvironments: ["air"],
+    availableInstallationTypesForMultiConductorLoad: ["A1", "A2", "B2", "E"],
+    availableInstallationTypesForOneConductorLoad: [],
+    availableNumberOfLoadedVeins: ["two", "multi"],
+    isolationMaterial: "PVC",
+    maxTemperature: 70,
+    material: "copper",
+  },
+  {
+    name: "YDYp",
+    availableEnvironments: ["air"],
+    availableInstallationTypesForMultiConductorLoad: ["A1", "A2", "B2", "E"],
+    availableInstallationTypesForOneConductorLoad: [],
+    availableNumberOfLoadedVeins: ["two", "multi"],
+    isolationMaterial: "PVC",
+    maxTemperature: 70,
+    material: "copper",
+  },
+  {
+    name: "YKY",
+    availableEnvironments: ["air", "ground"],
+    availableInstallationTypesForMultiConductorLoad: [
+      "A1",
+      "A2",
+      "B1",
+      "B2",
+      "E",
+      "D2",
+      "D1",
+      "F",
+    ],
+    availableInstallationTypesForOneConductorLoad: ["A1", "B1", "F", "D2"],
+    availableNumberOfLoadedVeins: ["one", "two", "multi"],
+    isolationMaterial: "PVC",
+    maxTemperature: 70,
+    material: "copper",
+  },
+  {
+    name: "YKXS",
+    availableEnvironments: ["air", "ground"],
+    availableInstallationTypesForMultiConductorLoad: [
+      "A1",
+      "A2",
+      "B2",
+      "E",
+      "D1",
+      "D2",
+    ],
+    availableInstallationTypesForOneConductorLoad: [
+      "A1",
+      "B1",
+      "F",
+      "D2",
+      "D1",
+    ],
+    availableNumberOfLoadedVeins: ["one", "two", "multi"],
+    isolationMaterial: "XLPE",
+    maxTemperature: 90,
+    material: "copper",
+  },
+  {
+    name: "YAKXS",
+    availableEnvironments: ["air", "ground"],
+    availableInstallationTypesForMultiConductorLoad: [
+      "A1",
+      "A2",
+      "B2",
+      "E",
+      "D1",
+      "D2",
+    ],
+    availableInstallationTypesForOneConductorLoad: [
+      "A1",
+      "B1",
+      "F",
+      "D2",
+      "D1",
+    ],
+    availableNumberOfLoadedVeins: ["one", "two", "multi"],
+    isolationMaterial: "XLPE",
+    maxTemperature: 90,
+    material: "aluminium",
+  },
+  {
+    name: "N2XH",
+    availableEnvironments: ["air"],
+    availableInstallationTypesForMultiConductorLoad: ["A1", "A2", "B2", "E"],
+    availableInstallationTypesForOneConductorLoad: ["A1", "B1", "F"],
+    availableNumberOfLoadedVeins: ["one", "two", "multi"],
+    isolationMaterial: "B2ca",
+    maxTemperature: 90,
+    material: "copper",
+  },
+];
+
 const FormPage = () => {
-  const [step, setStep] = useState(1);
-  const [formData, setFormData] = useState<initialDataProps>(initialData);
+  const [formData, setFormData] = useState<InitialDataProps>(initialData);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(formData);
   };
 
-  const nextStep = () => setStep((prev) => prev + 1);
-  const previousStep = () => setStep((prev) => prev - 1);
+  const getFilteredCables = () => {
+    let availableCables: CableType[] = cables;
 
-  const renderCurrentFormStep = (step: number) => {
-    switch (step) {
-      case 1:
-        return (
-          <TypeOfMetalDetails
-            handleInput={(e: React.ChangeEvent<HTMLSelectElement>) => {
-              setFormData((prevDetails) => ({
-                ...prevDetails,
-                typeOfMetal: e.target.value,
-              }));
-            }}
-            valueInput={formData.typeOfMetal}
-            nextStep={nextStep}
-          />
-        );
-      case 2:
-        return (
-          <TypeOfInsulatingMaterial
-            handleInput={(e: React.ChangeEvent<HTMLSelectElement>) => {
-              setFormData((prevDetails) => ({
-                ...prevDetails,
-                typeOfInsulatingMaterial: e.target.value,
-              }));
-            }}
-            valueInput={formData.typeOfInsulatingMaterial}
-            nextStep={nextStep}
-            previousStep={previousStep}
-          />
-        );
-      case 3:
-        return (
-          <NumberOfLoadedVeins
-            handleInput={(e: React.ChangeEvent<HTMLSelectElement>) => {
-              setFormData((prevDetails) => ({
-                ...prevDetails,
-                numberOfLoadedVeins: e.target.value,
-              }));
-            }}
-            valueInput={formData.numberOfLoadedVeins}
-            nextStep={nextStep}
-            previousStep={previousStep}
-          />
-        );
-      case 4:
-        return (
-          <InstalationMethod
-            handleInput={(e: React.ChangeEvent<HTMLSelectElement>) => {
-              setFormData((prevDetails) => ({
-                ...prevDetails,
-                instalationMethod: e.target.value,
-              }));
-            }}
-            valueInput={formData.instalationMethod}
-            nextStep={nextStep}
-            previousStep={previousStep}
-          />
-        );
-      case 5:
-        return (
-          <AmbientTemperature
-            handleInput={(e: React.ChangeEvent<HTMLSelectElement>) => {
-              setFormData((prevDetails) => ({
-                ...prevDetails,
-                ambientTemperature: e.target.value,
-              }));
-            }}
-            valueInput={formData.ambientTemperature}
-            nextStep={nextStep}
-            previousStep={previousStep}
-          />
-        );
-      case 6:
-        return (
-          <ThermalResistivityOfTheSoil
-            handleInput={(e: React.ChangeEvent<HTMLSelectElement>) => {
-              setFormData((prevDetails) => ({
-                ...prevDetails,
-                thermalResistivityOfTheSoil: e.target.value,
-              }));
-            }}
-            previousStep={previousStep}
-          />
-        );
+    if (formData.typeOfMetal) {
+      availableCables = availableCables.filter(
+        (cable) =>
+          cablesEnvironments.find((env) => env.name === cable)?.material ===
+          formData.typeOfMetal,
+      );
     }
+
+    if (formData.installationLocation) {
+      availableCables = availableCables.filter(
+        (cable) =>
+          cablesEnvironments
+            .find((env) => env.name === cable)
+            ?.availableEnvironments.includes(
+              formData.installationLocation as Environment,
+            ) ?? false,
+      );
+    }
+
+    if (formData.ambientTemperature) {
+      availableCables = availableCables.filter((cable) => {
+        const env = cablesEnvironments.find((env) => env.name === cable);
+        console.log(env?.maxTemperature, parseInt(formData.ambientTemperature));
+        return (
+          (env?.maxTemperature ?? 0) >= parseInt(formData.ambientTemperature)
+        );
+      });
+    }
+
+    if (formData.isolationMaterial) {
+      availableCables = availableCables.filter(
+        (cable) =>
+          cablesEnvironments.find((env) => env.name === cable)
+            ?.isolationMaterial === formData.isolationMaterial,
+      );
+    }
+
+    console.log(formData.numberOfLoadedVeins);
+    if (formData.numberOfLoadedVeins) {
+      availableCables = availableCables.filter(
+        (cable) =>
+          cablesEnvironments
+            .find((env) => env.name === cable)
+            ?.availableNumberOfLoadedVeins.includes(
+              formData.numberOfLoadedVeins as ConductorLoad,
+            ) ?? false,
+      );
+    }
+
+    if (formData.installationMethod) {
+      const isOneConductorLoad = formData.numberOfLoadedVeins === "one";
+      availableCables = availableCables.filter((cable) =>
+        isOneConductorLoad
+          ? cablesEnvironments
+              .find((env) => env.name === cable)
+              ?.availableInstallationTypesForOneConductorLoad.includes(
+                formData.installationMethod as InstallationType,
+              )
+          : cablesEnvironments
+              .find((env) => env.name === cable)
+              ?.availableInstallationTypesForMultiConductorLoad.includes(
+                formData.installationMethod as InstallationType,
+              ),
+      );
+    }
+
+    return availableCables;
+  };
+
+  const getAvailableIsolations = () => {
+    const availableCables = getFilteredCables();
+
+    const availableIsolations = availableCables
+      .map((cable) => cablesEnvironments.find((env) => env.name === cable))
+      .map((env) => env?.isolationMaterial)
+      .filter((val, index, self) => self.indexOf(val) === index);
+
+    return availableIsolations.map((iso) => ({
+      label: iso,
+      value: iso,
+    })) as Option[];
+  };
+
+  const getAvailableNumberOfLoadedVeins = () => {
+    const availableCables = getFilteredCables();
+
+    const availableEnvironments = cablesEnvironments.filter((env) =>
+      availableCables.includes(env.name as CableType),
+    );
+
+    const availableNumberOfLoadedVeins = [
+      ...new Set(
+        availableEnvironments
+          .map((env) => env.availableNumberOfLoadedVeins)
+          .flat(),
+      ),
+    ];
+
+    return availableNumberOfLoadedVeins.map((num) => ({
+      label: numberOfLoadedVeins.find((n) => n.value === num)?.label,
+      value: num,
+    })) as Option[];
+  };
+
+  const getAvailableInstallationTypes = () => {
+    const isOneConductorLoad = formData.numberOfLoadedVeins === "one";
+    const availableCables = getFilteredCables();
+    const availableEnvironments = cablesEnvironments.filter((env) =>
+      availableCables.includes(env.name as CableType),
+    );
+
+    const availableInstallationTypes = [
+      ...new Set(
+        availableEnvironments
+          .map((env) =>
+            isOneConductorLoad
+              ? env.availableInstallationTypesForOneConductorLoad
+              : env.availableInstallationTypesForMultiConductorLoad,
+          )
+          .flat(),
+      ),
+    ];
+
+    return availableInstallationTypes.map((type) => ({
+      label: type,
+      value: type,
+    })) as Option[];
   };
 
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
       <h1 className="pb-4 text-3xl">Formularz zgłoszeniowy</h1>
       <form onSubmit={handleSubmit} className="w-1/2">
-        {renderCurrentFormStep(step)}
+        {/* {renderCurrentFormStep(step)} */}
+        <Select
+          label="Wybierz tworzywo"
+          defaultOptionLabel="Wybierz tworzywo..."
+          onChange={(val) =>
+            setFormData({
+              ...formData,
+              typeOfMetal: val,
+              installationLocation: "",
+              isolationMaterial: "",
+              numberOfLoadedVeins: "",
+              installationMethod: "",
+              ambientTemperature: "",
+            })
+          }
+          options={metalTypes}
+          selectedValue={formData.typeOfMetal}
+        />
+        <Select
+          label="Wybierz lokalizacja montowania"
+          defaultOptionLabel="Wybierz lokalizacja montowania..."
+          onChange={(val) =>
+            setFormData({
+              ...formData,
+              installationLocation: val,
+              isolationMaterial: "",
+              numberOfLoadedVeins: "",
+              installationMethod: "",
+              ambientTemperature: "",
+            })
+          }
+          options={instalationLocations}
+          selectedValue={formData.installationLocation}
+        />
+        <Select
+          label="Wybierz temperature otoczenia"
+          defaultOptionLabel="Wybierz temperature otoczenia..."
+          onChange={(val) =>
+            setFormData({
+              ...formData,
+              ambientTemperature: val,
+              isolationMaterial: "",
+              numberOfLoadedVeins: "",
+              installationMethod: "",
+            })
+          }
+          options={temperatures.map((temp) => ({
+            label: temp + " C",
+            value: temp,
+          }))}
+          selectedValue={formData.ambientTemperature}
+        />
+        <Select
+          label="Wybierz materiał izolacyjny"
+          defaultOptionLabel="Wybierz materiał izolacyjny..."
+          onChange={(val) =>
+            setFormData({
+              ...formData,
+              isolationMaterial: val,
+              numberOfLoadedVeins: "",
+              installationMethod: "",
+            })
+          }
+          options={getAvailableIsolations()}
+          selectedValue={formData.isolationMaterial}
+        />
+        <Select
+          label="Wybierz liczbe zyl obciazonych"
+          defaultOptionLabel="Wybierz liczbe zyl obciazonych..."
+          onChange={(val) =>
+            setFormData({
+              ...formData,
+              numberOfLoadedVeins: val,
+              installationMethod: "",
+            })
+          }
+          options={getAvailableNumberOfLoadedVeins()}
+          selectedValue={formData.numberOfLoadedVeins}
+        />
+        <Select
+          label="Wybierz metode montowania"
+          defaultOptionLabel="Wybierz metode montowania..."
+          onChange={(val) =>
+            setFormData({
+              ...formData,
+              installationMethod: val,
+            })
+          }
+          options={getAvailableInstallationTypes()}
+          selectedValue={formData.installationMethod}
+        />
       </form>
+      {getFilteredCables().join(", ")}
     </div>
   );
 };
