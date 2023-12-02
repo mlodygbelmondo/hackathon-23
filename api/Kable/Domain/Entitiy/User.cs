57db7c6918ae7c5732a2aaaa914867895cd5f71e
JakubWaj <jw303269@student.polsl.pl>
2023-12-02 19:38:16 +0100

    Adding User Entity

---
+++ api/Kable/Domain/Entitiy/User.cs
@@ -0,0 +1,10 @@
+namespace Domain.Entitiy;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FistName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
